40b4585811f4b21fe76611e4368f05be9c8151d4
Aritra kr Basu <aritrabasu71@gmail.com>
2025-03-15 07:46:31 +0530

    dev folder added for debugging

---
+++ dev/src/server.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import payload from 'payload';
+import { seed } from "./seed";
+
+require('dotenv').config();
+const app = express();
+
+// Redirect root to Admin panel
+app.get('/', (_, res) => {
+  res.redirect('/admin');
+});
+
+const start = async () => {
+  // Initialize Payload
+  await payload.init({
+    secret: process.env.PAYLOAD_SECRET ?? '',
+    express: app,
+    onInit: async () => {
+      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
+    },
+  })
+
+  if (process.env.PAYLOAD_SEED === 'true') {
+    await seed(payload);
+  }
+
+  app.listen(4000);
+}
+
+start();
